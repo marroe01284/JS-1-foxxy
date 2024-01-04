5e9cdb8c9a2c585b49e3b02a1222c870076995ce
Hesham El Masry <heshamelmasry1992@gmail.com>
2024-01-04 14:18:49 +0100

    for loops and making decisions

---
+++ loops/js/script.js
@@ -0,0 +1,56 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// looping or iteration
+
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+
+
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i);
+}
+
+// i 1
+// 1 <= 5 true
+
+// console.log(1);
+
+// i = 2
+// 2 <= 5 /// true
+// console.log(2)
+
+// i = 3
+// 3 <= 5 true
+
+// console.log(3)
+
+
+// i = 4
+// 4 <= 5 // true
+
+// console.log(4)
+
+//  i + 1 = 4 +1 = 5
+
+// 5 <= 5// true
+
+// console.log(5);
+
+// i = 6
+
+
+// 6 <=5 false
+
+
+
+
+
