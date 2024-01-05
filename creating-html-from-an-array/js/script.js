b660dd5d04d8737444c64ba5fb37786f13b3fbae
Hesham El Masry <heshamelmasry1992@gmail.com>
2024-01-05 14:08:41 +0100

    create html from an array

--- creating-html-from-an-array/js/script.js
+++ creating-html-from-an-array/js/script.js
@@ -1,24 +1,53 @@
 const pets = [
     {
         type: null,
-        colour: "brown",
+        color: "brown",
         age: 10,
         friendly: true,
     },
     {
-        colour: "orange",
+        color: "orange",
         age: 7,
         friendly: false,
     },
     {
         type: "duck",
-        colour: "#00ff00",
+        color: "#00ff00",
         age: 20,
         friendly: false,
     },
 ];
 // Select the pet-container class
+const petContainer = document.querySelector(".pet-container");
+console.log("petContainer: ", petContainer);
 
-// Create a html variable to hold our html in it when we use the loop
+let html = "";
+
+// Create a html variable to
+// hold our html in it when we use the loop
+
+
 // Loop over the array
-// change the innerHTML
+// change the innerHTML of the pet-container class div
+
+const totalItemsOfPets = pets.length;
+
+console.log("totalItemsOfPets: ", totalItemsOfPets);
+
+//
+for (let i = 0; i < totalItemsOfPets; i++) {
+    console.log(pets[i].color);
+    console.log(pets[i].age);
+
+    html += `
+                <div class="pet pet-detail">
+                  <p style="color: ${pets[i].color}" >Colour</p>
+                  <h2>Age: ${pets[i].age}</h2>
+                  <p>Friendly : ${pets[i].friendly ? "Yes it is friendly" : "no it is not friendly"}</p>
+                </div>
+    `;
+    console.log(html);
+}
+
+petContainer.innerHTML = html;
+
