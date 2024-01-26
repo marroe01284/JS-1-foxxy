895e0864186eae6e297ef32d03c74ad83b52d760
Hesham El Masry <heshamelmasry1992@gmail.com>
2024-01-26 12:27:37 +0100

    arrow fucntion

---
+++ playing-with-functions/js/script.js
@@ -0,0 +1,20 @@
+// Variable functions
+
+showPetNameTwo("fdf")
+const showPetName = function (name) {
+    console.log("the pet name is:  " + name);
+}
+
+const showPetName = (name) => {
+    console.log("the pet name is:  " + name);
+}
+
+// Fat Arrow Functions
+
+
+
+function showPetNameTwo (name){
+    console.log("the pet name is:  " + name);
+}
+
+showPetName("Mailio");
