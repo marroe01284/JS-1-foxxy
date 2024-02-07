59e5c3efad6d3f08b4ab12b2b34d9fe528cfe243
deanvons <deanvons@gmail.com>
2024-02-07 13:14:32 +0100

    feat: added simple loading

--- shelf-help-demo-build/app.js
+++ shelf-help-demo-build/app.js
@@ -1,17 +1,20 @@
 // !! Need the shelf help JSON server to be running on http://localhost:3000
 
-// fetch the book data.
 const bookListDiv = document.getElementById("book-list");
 let bookData = [];
-
+// fetch the book data.
 fetch("http://localhost:3000/books")
-  .then((response) => response.json())
+  .then((response) => {
+    return response.json();
+  })
   .then((bookResultData) => {
     bookData = bookResultData;
 
     for (const book of bookData) {
       displayBook(book);
     }
+
+    document.getElementById("loader").remove();
   });
 
 // function to create and insert the HTML to display a book
