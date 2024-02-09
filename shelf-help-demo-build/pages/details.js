2ee181ca77c33d5d2ea3292f599519e5f4ecf3c3
deanvons <deanvons@gmail.com>
2024-02-09 15:18:50 +0100

    feat: added blurb to book details

--- shelf-help-demo-build/pages/details.js
+++ shelf-help-demo-build/pages/details.js
@@ -1,22 +1,21 @@
 const selectedBook = JSON.parse(sessionStorage.getItem('selectedBook'))
 const bookListDiv = document.getElementById("book-list");
 
-displayBook(selectedBook)
+displayBookDetails(selectedBook)
 
 // Defines a function to display a book's information in the DOM
-function displayBook(book) {
+function displayBookDetails(book) {
     const bookDiv = document.createElement("div");
     const bookTitlePara = document.createElement("p");
     bookTitlePara.innerText = book.title;
     const bookImg = document.createElement("img");
     bookImg.src = `http://localhost:3000/assets/images/${book.coverImg}`;
     bookImg.alt = "a picture of a book";
-    bookImg.addEventListener('click',()=>{
-      // navigate to details page
-      sessionStorage.setItem("selectedBook",JSON.stringify(book))
-      window.location.href = "http://127.0.0.1:5500/shelf-help-demo-build/pages/details.html"
-    })
+    const bookBlurb = document.createElement('p')
+    bookBlurb.innerText = book.blurb
+  
     bookDiv.appendChild(bookImg);
     bookDiv.appendChild(bookTitlePara);
+    bookDiv.appendChild(bookBlurb)
     bookListDiv.appendChild(bookDiv);
   }
